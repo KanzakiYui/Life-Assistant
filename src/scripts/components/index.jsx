07bffa324a13805240b700a4a04898c4debd0cdb
KanzakiYui <yaopeng.liu@mail.utoronto.ca>
2019-08-07 16:42:59 -0400

    update

--- src/scripts/components/index.jsx
+++ src/scripts/components/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import MUIThemeTester from 'test/mui-theme-tester'
 
-const Application = () => {
-    return (
-        <React.Fragment>
-            <MUIThemeTester />
-        </React.Fragment>
-    )
-}
+const Application = () =>
+    <React.Fragment>
+        <MUIThemeTester />
+    </React.Fragment>
+
 
 export default Application
