482dc676a0b903689a991050494047e128204c5b
KanzakiYui <yaopeng.liu@mail.utoronto.ca>
2019-08-07 16:47:22 -0400

    Update

--- src/scripts/mui/theme/breakpoints.js
+++ src/scripts/mui/theme/breakpoints.js
@@ -1,10 +1,14 @@
+/**
+ * xs = 0, sm = 600 are default setting, while 1025, 1441, 1921 are
+ * workaround to present >1024, >1440, >1920
+ */
 const breakpoints = {
     values: {
-        xs: 0, // >=0      (default)
-        sm: 600, // >=600    (default)
-        md: 1025, // >=1025   (>1024)
-        lg: 1441, // >=1441   (>1440)
-        xl: 1921 // >=1921   (>1920)
+        xs: 0,
+        sm: 600,
+        md: 1025,
+        lg: 1441,
+        xl: 1921
     }
 }
 
