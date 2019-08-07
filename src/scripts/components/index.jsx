f794b14dc96268158ec5d179976a250fbade5f6f
KanzakiYui <yaopeng.liu@mail.utoronto.ca>
2019-08-07 11:55:29 -0400

    Solved palette.js error,  added more test, restructured files

---
+++ src/scripts/components/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MUIThemeTester from './test'
+
+const Application = () => {
+    return (
+        <React.Fragment>
+            <MUIThemeTester />
+        </React.Fragment>
+    )
+}
+
+export default Application
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,32 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-import { Box, Typography, CssBaseline } from '@material-ui/core'
+import { CssBaseline } from '@material-ui/core'
 import { ThemeProvider } from '@material-ui/styles'
 
 import theme from 'mui/theme'
-import { baseSpacing, baseHTMLFontSize, scaler } from 'mui/theme/constants'
-
-const Test = () =>
-    <Box>
-        <Box mb={1}>
-            <Typography>Current Spacing: {baseSpacing * scaler}</Typography>
-        </Box>
-        <Box mb={1}>
-            <Typography>
-                Current Font Size: {baseHTMLFontSize * scaler}
-            </Typography>
-        </Box>
-        <Box mb={1}>
-            <Typography>Scaler: {scaler}</Typography>
-        </Box>
-    </Box>
-
+import Application from 'components'
 
 render(
     <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Test />
+        <Application />
     </ThemeProvider>,
     document.getElementById('react-root')
 )
