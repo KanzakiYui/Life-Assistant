f04f83a9a4dedd5029cd8cdcb133ec3885fbb009
Yui <yaopeng.liu@mail.utoronto.ca>
2019-08-11 19:02:59 -0400

    Added scale option for custom component - button (#5)

--- src/scripts/mui/components/button.jsx
+++ src/scripts/mui/components/button.jsx
@@ -5,6 +5,7 @@ import { makeStyles } from '@material-ui/styles'
 
 const useStyles = makeStyles(theme => ({
     root: {
+        'transform': ({ size }) => `scale(${size})`,
         'backgroundColor': ({ color }) => theme.palette[color].main,
         'padding': `${theme.spacing(0.5)}px ${theme.spacing(4)}px`,
         'minWidth': theme.typography.fontSize * 16,
@@ -45,14 +46,22 @@ const useStyles = makeStyles(theme => ({
     }
 }))
 
-const Button = ({ outlined, color: color, ...others }) => {
+const Button = ({ outlined, color: color, scale, ...others }) => {
     const ButtonColor = color || 'default'
-    const classes = useStyles({ color: ButtonColor })
+    const size = scale
+        ? scale === 'sm'
+            ? 0.75
+            : scale === 'lg'
+                ? 1.25
+                : scale
+        : 1
+    const classes = useStyles({ color: ButtonColor, size })
     const variant = outlined ? 'outlined' : 'contained'
     return <MuiButton classes={classes} variant={variant} {...others} />
 }
 
 Button.propTypes = {
+    scale: PropType.oneOfType([ PropType.string, PropType.number ]),
     outlined: PropType.bool,
     color: PropType.string
 }
